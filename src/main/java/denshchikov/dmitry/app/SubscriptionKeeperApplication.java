bee863256e683437fec9a5fb4a91b1e3939e7ef0
Dmitry Denshchikov <mr.dd33@yandex.ru>
2024-05-04 11:49:56 +0300

    Initial commit

---
+++ src/main/java/denshchikov/dmitry/app/SubscriptionKeeperApplication.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/denshchikov/dmitry/app/SubscriptionKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
